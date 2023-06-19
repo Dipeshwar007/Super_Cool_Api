9abd76b743c0135dde443013da6a4989dd55a2d5
Dipeshwar007 <tunashah10@gmail.com>
2023-06-19 11:00:30 +0545

    Atlas bug fix

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ db.connect("mongodb://127.0.0.1:27017/warehouse").then(()=>{
 
 // Mongodb Atlas
 
-db.connect("mongodb+srv://tunashah10:va5sW8EQewxMeX4q@warehouse.3zcxr4f.mongodb.net/").then(()=>{
+db.connect("mongodb+srv://tunashah10:va5sW8EQewxMeX4q@warehouse.3zcxr4f.mongodb.net/test").then(()=>{
     console.log("Database connected ...")
 }).catch((err)=>{
     console.log(`Database Not connected :- ${err}`)
