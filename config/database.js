122037bfab260cab117f8128aac28a39b3f0e5b8
Dipeshwar007 <tunashah10@gmail.com>
2023-06-19 11:32:11 +0545

    test atlas

--- config/database.js
+++ config/database.js
@@ -11,8 +11,6 @@ db.connect("mongodb://127.0.0.1:27017/warehouse").then(()=>{
 })
 */
 
-
-
 // Mongodb Atlas
 
 db.connect("mongodb+srv://tunashah10:va5sW8EQewxMeX4q@warehouse.3zcxr4f.mongodb.net/test").then(()=>{
